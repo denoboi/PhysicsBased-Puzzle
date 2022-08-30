105a7e094724e85cd9d8ac820dc72d53fd4b04ce
denoboi <deniz.kaymakci@hotmail.com>
2022-08-31 00:14:47 +0300

    GameManager.cs
    Ball reset if hit the trap, Ball and Camera movement

--- PuzzleGame/Assets/[PhysicsPuzzle]/Scripts/GameManager.cs
+++ PuzzleGame/Assets/[PhysicsPuzzle]/Scripts/GameManager.cs
@@ -9,6 +9,13 @@ public class GameManager : MonoBehaviour
 {
     private int _score;
     private int _lives = 3;
+    
+    [Header("Ball")]
+    public GameObject Ball;
+    private Rigidbody2D _ballRb;
+    private Vector2 _ballStartPos;
+    private float _ballDistanceToCam;
+    private float _lastYPos; //Moving ball stuff
 
     public TextMeshProUGUI ScoreText, LivesText;
     
@@ -20,14 +27,24 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        _ballStartPos = Ball.transform.position;
+       
+        _lastYPos = _ballStartPos.y;
+        _ballRb = Ball.GetComponent<Rigidbody2D>();
         UpdateTextElements();
     }
 
     public void GameOver()
     {
+        //resetting ball position
+        _ballStartPos.y = Camera.main.transform.position.y + _ballStartPos.y; //kamera hep asagi hareket edecegi icin gerek olacak.
+        Ball.transform.position = _ballStartPos; //y pozisyonunu overwrite ettik 
+        _ballRb.velocity = Vector2.zero; //In order to prevent fall speed when the game starts.
+        
         _lives--;
-        if (_lives == 0)
+        if (_lives <= 0)
         {
+            _ballRb.isKinematic = true;
             print("GameOver");
         }
         
@@ -48,6 +65,19 @@ public class GameManager : MonoBehaviour
         LivesText.text = "Lives: " + _lives;
     }
 
-   
-    
+    private void LateUpdate() //update'den daha gec ve daha az cagriliyor.
+    {
+        MoveCamWithTheBall();
+    }
+
+    void MoveCamWithTheBall()
+    {
+        if (Ball.transform.position.y <= Camera.main.transform.position.y)
+        {
+            Vector3 oldCamPos = Camera.main.transform.position;
+            Vector3 newCamPos = new Vector3(oldCamPos.x, oldCamPos.y - 1f, oldCamPos.z);
+
+            Camera.main.transform.position = Vector3.Lerp(oldCamPos, newCamPos, 2 * Time.deltaTime);
+        }
+    }
 }
